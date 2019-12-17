c1d942588000cdc67c729883f4f933a359183b72
eliasand <elias.andersson2@elev.ga.ntig.se>
2019-12-17 11:14:53 +0100

    lektion slut

--- ad-del2.py
+++ ad-del2.py
@@ -1,24 +1,20 @@
 import csv, random, string, subprocess, platform
-chars = "abcdefghijklmnopqrstuvwxyz"
-chars1 = "1234567890"
-chars2 = "!%&/()=#"
-import random
-import string
-password = ""
 
-for c in range (2):
-    password += random.choice(chars)
-    password += random.choice(chars.upper())
-    password += random.choice(chars1)
-    password += random.choice(chars2)
+def lösenGen():
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    chars1 = "1234567890"
+    chars2 = "!%&/()=#"
 
-password = ''.join(random.sample(password,len(password)))
-#f= open("losenordfil.txt","a") 
-#f.write(password)
-#f.write("\n")
+    for c in range (2):
+        password = ""
+        password += random.choice(chars)
+        password += random.choice(chars.upper())
+        password += random.choice(chars1)
+        password += random.choice(chars2)
 
-print("Password: ", (password))
-
+    password = ''.join(random.sample(password,len(password)))
+password = lösenGen()
+print("password" + str(password))
 fields = ['user', "password"] 
 
 rows = [ ['Nikhil', (password)], 
@@ -47,20 +43,13 @@ with open(filename, 'r') as csvfile:
     # creating a csv reader object 
     csvreader = csv.reader(csvfile) 
 
- 
-
-
- 
     for row in csvreader: 
         rows.append(row) 
 
- 
     print("Total no. of rows: %d"%(csvreader.line_num)) 
-
  
 print('Field names are:' + ', '.join(field for field in fields)) 
 
- 
 print('\nFirst 5 rows are:\n') 
 for row in rows[:5]: 
 
